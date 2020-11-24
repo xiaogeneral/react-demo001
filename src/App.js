590499e9d3b993272843fbb2a0bdadb6d4e45794
yangdongliang002 <yangdongliang002@ke.com>
2020-11-24 17:02:19 +0800

    表单校验

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 // import './App.css';
 // import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import TodoList from './pages/TodoList';
+// import TodoList from './pages/TodoList';
 // import ConnectTodoList from './pages/TodoList/connect'
 import SiderDemo from './layout'
+import FormValidate from './pages/FormValidate';
+import DynamicRue from './pages/FormValidate/DynamicRue';
 
 
 function App() {
   return (
     <div className="App">
       <SiderDemo>
-        <TodoList />
+        <FormValidate />
+        <DynamicRue />
       </SiderDemo>
     </div>
   );
