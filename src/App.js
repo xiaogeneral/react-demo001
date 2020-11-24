8b95572ded94b4cd1c9aa98dca641269df89f765
yangdongliang002 <yangdongliang002@ke.com>
2020-11-24 11:02:14 +0800

    表单验证

--- src/App.js
+++ src/App.js
@@ -1,39 +1,17 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import { Provider } from 'react-redux'
+// import './App.css';
+// import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
 import TodoList from './pages/TodoList';
-import ConnectTodoList from './pages/TodoList/connect'
-import store from './store'
+// import ConnectTodoList from './pages/TodoList/connect'
+import SiderDemo from './layout'
+
 
 function App() {
   return (
     <div className="App">
-      <Provider store={store}>
-        <BrowserRouter>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/redux">redux</Link>
-                </li>
-                <li>
-                  <Link to="/react-redux">react-redux</Link>
-                </li>
-              </ul>
-            </nav>
-
-            <Switch>
-              <Route path="/redux">
-                <TodoList />
-              </Route>
-              <Route path="/react-redux">
-                <ConnectTodoList />
-              </Route>
-            </Switch>
-          </div>
-        </BrowserRouter>
-      </Provider>
+      <SiderDemo>
+        <TodoList />
+      </SiderDemo>
     </div>
   );
 }
