ce33628f0b8152a4420a0edd209d71195c9422bf
yangdongliang002 <yangdongliang002@ke.com>
2020-12-02 16:27:56 +0800

    cc

--- _githooks.js
+++ _githooks.js
@@ -3,8 +3,16 @@ const child_process = require('child_process');
 function test() {
   console.log('ddddd');
   // eslint-disable-next-line no-unused-expressions
-  const branchName = child_process.execSync('git name-rev --name-only HEAD', {'encoding': 'utf8'});
+  const branchName = child_process.execSync(
+    'git name-rev --name-only HEAD',
+    {'encoding': 'utf8'}
+    );
   console.log(branchName, 'branchName')
-  process.exitCode = 0
+  if (branchName === 'feature/form-validate') {
+    process.exitCode = 0
+  } else {
+    process.exitCode = 0
+  }
+
 }
 test()
