a9224f68c669b9e8ff1f28b0d954d7cda7288a11
yangdongliang002 <yangdongliang002@ke.com>
2020-12-02 16:19:00 +0800

    execsync

--- _githooks.js
+++ _githooks.js
@@ -1,6 +1,10 @@
+#!/usr/bin/env node
+const child_process = require('child_process');
 function test() {
   console.log('ddddd');
   // eslint-disable-next-line no-unused-expressions
+  const branchName = child_process.execSync('git name-rev --name-only HEAD', {'encoding': 'utf8'});
+  console.log(branchName, 'branchName')
   process.exitCode = 0
 }
 test()
