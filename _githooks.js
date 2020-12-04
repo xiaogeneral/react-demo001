b9ac5dde91f655c87ffea3ff3245576ea13d93a2
yangdongliang002 <yangdongliang002@ke.com>
2020-12-04 19:00:19 +0800

    添加提示

--- _githooks.js
+++ _githooks.js
@@ -6,10 +6,7 @@ function test() {
     {'encoding': 'utf8'}
     );
   if (branchName.includes('feature/form-validate')) {
-    console.log('禁止以命令行在master上merge')
-    child_process.execSync(
-      'git merge --abort'
-    );
+    console.log('禁止以命令行在master上merge\n你可以用git merge --abort来消除本次操作造成的影响')
     process.exitCode = 1;
   } else {
     process.exitCode = 0
