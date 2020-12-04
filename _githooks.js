de023524cb1efc931878f52a288c292622dd17dd
yangdongliang002 <yangdongliang002@ke.com>
2020-12-04 18:50:37 +0800

    优化代码

--- _githooks.js
+++ _githooks.js
@@ -1,14 +1,10 @@
 #!/usr/bin/env node
 const child_process = require('child_process');
 function test() {
-  console.log('ddddd');
-  // eslint-disable-next-line no-unused-expressions
   const branchName = child_process.execSync(
     'git name-rev --name-only HEAD',
     {'encoding': 'utf8'}
     );
-  console.log(branchName.length, 'branchName')
-  console.log(typeof branchName, 'branchName type')
   if (branchName.includes('feature/form-validate')) {
     console.log('禁止以命令行在master上merge')
     child_process.execSync(
@@ -16,7 +12,6 @@ function test() {
     );
     process.exitCode = 1;
   } else {
-    console.log('er han')
     process.exitCode = 0
   }
 
